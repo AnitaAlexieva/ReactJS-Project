ecb9dabaa6b74c87aba7c1e9af37421c1f34b3cb
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-03-19 10:57:21 +0200

    Create login form in navigation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 import About from "./components/about/About"
+import DetailsRecipe from "./components/detailsRecipe/DetailsRecipe"
 import Footer from "./components/footer.jsx/Footer"
 import Header from "./components/header/Header"
 import Home from "./components/home/Home"
+import Login from "./components/login/Login"
 import Recipes from "./components/recipes/Recipes"
 import {Route, Routes} from 'react-router'
 
@@ -15,6 +17,8 @@ function App() {
             <Route index element={<Home/>} />
             <Route path="/about" element={<About />} />
             <Route path="/recipes" element={<Recipes/>}/>
+            <Route path="/recipesDetails" element={<DetailsRecipe/>}/>
+            <Route path="/login" element={<Login/>}/>
          </Routes>
   
       <Footer/>
