99cf6c81daf91e704187b454c1cf92cf097f1ed6
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-03-31 21:51:20 +0300

    Add create book page

---
+++ client/src/components/create-book/CreateBook.jsx
@@ -0,0 +1,32 @@
+import './create-book.css';
+import { useNavigate } from 'react-router';
+import { useCreateBook } from '../../api/bookApi';
+
+export default function CreateRecipeBook() {
+  const navigate = useNavigate();
+  const { create } = useCreateBook();
+
+  const submitAction = async (formData) => {
+    const recipeBookData = Object.fromEntries(formData);
+    await create(recipeBookData);
+    navigate('/books');
+  };
+
+  return (
+    <section className="create">
+      <div className="create-form">
+        <h2>Create a Recipe Book</h2>
+        <form action={submitAction}>
+          <input type="text" name="title" placeholder="Recipe Book Title" required />
+          <input type="text" name="author" placeholder="Author" required />
+          <input type="text" name="genre" placeholder="Cuisine Type" required />
+          <input type="text" name="publicationYear" placeholder="Publication Year" required />
+          <input type="number" name="price" placeholder="Price" required />
+          <input type="text" name="imageUrl" placeholder="Cover Image URL" required />
+          <textarea name="description" placeholder="Book Description" rows="5" required></textarea>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </section>
+  );
+}
