8b1d9dbd6e94183a31bfb3f01e9e9f6fe56bc3bc
Anita Alexieva :) <anitaalexievaa@gmail.com>
2025-04-02 14:42:29 +0300

    Make small changes

--- client/src/components/about/About.jsx
+++ client/src/components/about/About.jsx
@@ -9,12 +9,14 @@ export default function About() {
                  <div className="about_taital_main">
                     <div className="about_taital">About Us</div>
                     <p className="about_text">Full cleaning and housekeeping services for companies and households.</p>
-                    <p className="about_text">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.Lorem Ipsum is simply.Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.Lorem Ipsum is simply</p>
+                    <p className="about_text">Welcome to Taste Book – your ultimate destination for culinary creativity and inspiration! Whether you’re a novice in the kitchen or a seasoned chef, we’ve got something for everyone.
+
+                     Our mission is simple: to bring people together through the joy of cooking. Here, you can explore a vast collection of mouthwatering recipes, create your own personalized recipe books, and discover new meals that match your taste. </p>
                     <div className="read_bt"><Link to="/recipes">Our recipes</Link></div>
                  </div>
               </div>
               <div className="col-md-6">
-                 <div className="about_img"><img src="/images/about.jpg"/></div>
+                 <div className="about_img"><img className="abt-img" src="/images/about.jpg"/></div>
               </div>
            </div>
         </div>
